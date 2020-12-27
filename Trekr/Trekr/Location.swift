bc065f3f8e97b235b9bacf76e90b64d756a81144
Pawan Kumar <pawan.kumar@Apples-MacBook-Pro.local>
2020-12-27 22:39:51 +0530

    Initial Setupp: First Screen

---
+++ Trekr/Trekr/Location.swift
@@ -0,0 +1,11 @@
+//
+//  Location.swift
+//  Trekr
+//
+//  Created by Pawan Kumar on 27/12/20.
+//
+
+import Foundation
+struct Location {
+    
+}
---
+++ Trekr/Trekr/TrekrApp.swift
@@ -0,0 +1,19 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Pawan Kumar on 27/12/20.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+            ContentView()
+            }
+        }
+    }
+}
